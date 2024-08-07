a7d122a2641c91f081f97405be16824837808d34
Jaimin Mevada <jaiminkumarmevada@gmail.com>
2024-08-06 16:37:58 -0800

    First loop question

---
+++ First-loop.js
@@ -0,0 +1,13 @@
+/*   *****
+     *****
+     *****
+     *****
+     *****   */
+
+     for (let i = 0; i < 5; i++) { // Loop for rows
+        let row = "";
+        for (let j = 0; j < 5; j++) { // Loop for columns
+            row += "*";
+        }
+        console.log(row); // Print each row
+    }
